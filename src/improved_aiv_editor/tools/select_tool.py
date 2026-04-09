de20e9fdb76c6d7b5054cb927a3fb2cea42261d6
Renke Hohl <renke.hohl@schmidt-embedded-systems.de>
2026-04-09 16:57:24 +0200

    Implement copy-paste by holding option and drag

--- src/improved_aiv_editor/tools/select_tool.py
+++ src/improved_aiv_editor/tools/select_tool.py
@@ -6,10 +6,13 @@ from typing import TYPE_CHECKING, Optional
 
 from PyQt6.QtCore import QPointF, Qt, QRectF
 from PyQt6.QtGui import QMouseEvent, QKeyEvent, QUndoStack, QPen, QBrush, QColor
-from PyQt6.QtWidgets import QGraphicsRectItem, QGraphicsView
+from PyQt6.QtWidgets import QGraphicsRectItem, QGraphicsView, QGraphicsItem
 
 from improved_aiv_editor.tools.base_tool import BaseTool
-from improved_aiv_editor.models.commands import MoveBuildingsCommand, DeleteFramesCommand, DeletePositionsCommand
+from improved_aiv_editor.models.commands import (
+    MoveBuildingsCommand, DeleteFramesCommand, DeletePositionsCommand,
+    PlaceBuildingCommand,
+)
 from improved_aiv_editor.views.map_canvas import (
     BuildingGraphicsItem, GatehouseGraphicsItem, WallSegmentItem, KeepGraphicsItem,
     TILE_SIZE, MapScene, MapCanvas,
@@ -35,6 +38,9 @@ class SelectTool(BaseTool):
         self._drag_items: list[BuildingGraphicsItem | GatehouseGraphicsItem | WallSegmentItem | KeepGraphicsItem] = []
         self._drag_start_tile: Optional[tuple[int, int]] = None
 
+        self._clone_drag: bool = False
+        self._clone_ghosts: list[QGraphicsItem] = []
+
         self._rubber_origin: Optional[QPointF] = None
         self._rubber_additive: bool = False
         self._rubber_item: Optional[QGraphicsRectItem] = None
@@ -47,7 +53,29 @@ class SelectTool(BaseTool):
         super().deactivate()
         self._canvas.setDragMode(QGraphicsView.DragMode.NoDrag)
         self._dragging = False
+        self._clear_clone_ghosts()
         self._clear_rubber_band()
+
+    def _create_clone_ghosts(self) -> None:
+        """Create semi-transparent copies of drag items for visual clone feedback."""
+        self._clear_clone_ghosts()
+        for item in self._drag_items:
+            rect = item.boundingRect()
+            ghost = QGraphicsRectItem(rect)
+            brush = item.brush()
+            color = QColor(brush.color())
+            color.setAlpha(100)
+            ghost.setBrush(QBrush(color))
+            ghost.setPen(QPen(color.darker(130), 1.0, Qt.PenStyle.DashLine))
+            ghost.setPos(item.pos())
+            ghost.setZValue(15000)
+            self._scene.addItem(ghost)
+            self._clone_ghosts.append(ghost)
+
+    def _clear_clone_ghosts(self) -> None:
+        for ghost in self._clone_ghosts:
+            self._scene.removeItem(ghost)
+        self._clone_ghosts.clear()
 
     def _clear_rubber_band(self) -> None:
         if self._rubber_item is not None:
@@ -84,12 +112,15 @@ class SelectTool(BaseTool):
                 item.setSelected(True)
 
             self._dragging = True
+            self._clone_drag = bool(event.modifiers() & Qt.KeyboardModifier.AltModifier)
             self._drag_start = scene_pos
             self._drag_start_tile = self._scene.scene_to_tile(scene_pos)
             self._drag_items = [
                 i for i in self._scene.selectedItems()
                 if isinstance(i, (BuildingGraphicsItem, GatehouseGraphicsItem, WallSegmentItem, KeepGraphicsItem))
             ]
+            if self._clone_drag:
+                self._create_clone_ghosts()
         else:
             self._dragging = False
             self._rubber_additive = bool(event.modifiers() & Qt.KeyboardModifier.ControlModifier)
@@ -105,11 +136,18 @@ class SelectTool(BaseTool):
         if self._dragging and self._drag_start is not None:
             dx = scene_pos.x() - self._drag_start.x()
             dy = scene_pos.y() - self._drag_start.y()
-            for item in self._drag_items:
-                item.setPos(
-                    item.tile_x * TILE_SIZE + dx,
-                    item.tile_y * TILE_SIZE + dy,
-                )
+            if self._clone_ghosts:
+                for ghost, item in zip(self._clone_ghosts, self._drag_items):
+                    ghost.setPos(
+                        item.tile_x * TILE_SIZE + dx,
+                        item.tile_y * TILE_SIZE + dy,
+                    )
+            else:
+                for item in self._drag_items:
+                    item.setPos(
+                        item.tile_x * TILE_SIZE + dx,
+                        item.tile_y * TILE_SIZE + dy,
+                    )
             return
 
         if self._rubber_origin is not None:
@@ -126,22 +164,31 @@ class SelectTool(BaseTool):
             dtx = end_tile[0] - self._drag_start_tile[0]
             dty = end_tile[1] - self._drag_start_tile[1]
 
-            if dtx != 0 or dty != 0:
-                moves: list[tuple[int, int, int, int]] = []
-                for item in self._drag_items:
-                    moves.append((item.frame_id, item.pos_index, dtx, dty))
+            clone = self._clone_drag or bool(
+                event.modifiers() & Qt.KeyboardModifier.AltModifier
+            )
 
-                if moves:
-                    cmd = MoveBuildingsCommand(self._document, moves)
-                    self._undo_stack.push(cmd)
+            if dtx != 0 or dty != 0:
+                if clone:
+                    self._finish_clone_drag(dtx, dty)
                 else:
+                    moves: list[tuple[int, int, int, int]] = []
                     for item in self._drag_items:
-                        item.setPos(item.tile_x * TILE_SIZE, item.tile_y * TILE_SIZE)
+                        moves.append((item.frame_id, item.pos_index, dtx, dty))
+
+                    if moves:
+                        cmd = MoveBuildingsCommand(self._document, moves)
+                        self._undo_stack.push(cmd)
+                    else:
+                        for item in self._drag_items:
+                            item.setPos(item.tile_x * TILE_SIZE, item.tile_y * TILE_SIZE)
             else:
                 for item in self._drag_items:
                     item.setPos(item.tile_x * TILE_SIZE, item.tile_y * TILE_SIZE)
 
+            self._clear_clone_ghosts()
             self._dragging = False
+            self._clone_drag = False
             self._drag_start = None
             self._drag_items = []
             self._drag_start_tile = None
@@ -150,6 +197,29 @@ class SelectTool(BaseTool):
             rect = QRectF(self._rubber_origin, scene_pos).normalized()
             self._scene.select_buildings_in_rect(rect, self._rubber_additive)
             self._clear_rubber_band()
+
+    def _finish_clone_drag(self, dtx: int, dty: int) -> None:
+        """Create duplicated frames at the offset position."""
+        by_frame: dict[int, list[tuple[int, int]]] = {}
+        for item in self._drag_items:
+            if item.building_def.id == 61:
+                continue
+            by_frame.setdefault(item.frame_id, []).append(
+                (item.tile_x + dtx, item.tile_y + dty)
+            )
+
+        if not by_frame:
+            return
+
+        self._undo_stack.beginMacro("Duplicate Buildings")
+        for frame_id, new_positions in by_frame.items():
+            frame = self._document.frame_by_id(frame_id)
+            cmd = PlaceBuildingCommand(
+                self._document, frame.item_type, new_positions,
+                should_pause=frame.should_pause,
+            )
+            self._undo_stack.push(cmd)
+        self._undo_stack.endMacro()
 
     def on_key(self, event: QKeyEvent) -> None:
         if event.key() in (Qt.Key.Key_Delete, Qt.Key.Key_Backspace):
